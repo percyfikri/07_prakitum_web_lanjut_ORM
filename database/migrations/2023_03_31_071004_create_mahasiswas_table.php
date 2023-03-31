e875344d8ec093a2196ab7692c3366800e03eec5
percyfikri <mafikri27@gmail.com>
2023-03-31 14:45:39 +0700

    migration : create table mahasiswa

--- database/migrations/2023_03_31_071004_create_mahasiswas_table.php
+++ database/migrations/2023_03_31_071004_create_mahasiswas_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Validation\Rules\Unique;
 
 return new class extends Migration
 {
@@ -12,8 +13,13 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('mahasiswas', function (Blueprint $table) {
-            $table->id();
+            $table->string('nim')->primary();          
+            $table->string('nama', 20);
+            $table->string('kelas', 20);
+            $table->string('jurusan', 20);
+            $table->bigInteger('no_handphone');
             $table->timestamps();
+
         });
     }
 
