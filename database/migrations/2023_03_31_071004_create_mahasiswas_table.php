a56dbf0f974ae0c80621128580a33c2647eb2f70
percyfikri <mafikri27@gmail.com>
2023-04-01 22:45:47 +0700

    Revert "(migration): create table mahasiswa"

--- database/migrations/2023_03_31_071004_create_mahasiswas_table.php
+++ database/migrations/2023_03_31_071004_create_mahasiswas_table.php
@@ -12,12 +12,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('mahasiswas', function (Blueprint $table) {
-            $table->unsignedInteger('Nim')->primary();          
-            $table->string('Nama', 20);
-            $table->string('Kelas', 20);
-            $table->string('Jurusan', 20);
-            $table->bigInteger('No_handphone');
+        Schema::create('mahasiswa', function (Blueprint $table) {
+            $table->integer('nim')->primary();          
+            $table->string('nama', 20);
+            $table->string('kelas', 20);
+            $table->string('jurusan', 20);
+            $table->bigInteger('no_handphone');
             $table->timestamps();
 
         });
@@ -28,6 +28,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('mahasiswas');
+        Schema::dropIfExists('mahasiswa');
     }
 };
--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('mahasiswas', MahasiswaController::class);
+Route::resource('mahasiswa', MahasiswaController::class);
