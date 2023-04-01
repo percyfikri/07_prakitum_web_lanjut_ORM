001dde9e274be984614a32714fda6a4a084eab62
percyfikri <mafikri27@gmail.com>
2023-04-01 22:16:45 +0700

    (models): setting value of fillable

--- app/Models/Mahasiswa.php
+++ app/Models/Mahasiswa.php
@@ -5,7 +5,26 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Mahasiswa extends Model
-{
-    use HasFactory;
-}
+// class Mahasiswa extends Model
+// {
+//     use HasFactory;
+// }
+
+class Mahasiswa extends Model //Definisi Model 
+{     
+    protected $table="mahasiswas"; // Eloquent akan membuat model mahasiswa menyimpan record di tabel mahasiswas     
+    public $timestamps= false;  
+    protected  $primaryKey = 'nim'; // Memanggil isi DB Dengan primarykey     
+    /** 
+     * The attributes that are mass assignable. 
+     * 
+     * @var array      */ 
+    protected $fillable = [ 
+        'Nim', 
+        'Nama', 
+        'Kelas', 
+        'Jurusan', 
+        'No_Handphone', 
+    ]; 
+}; 
+ 
