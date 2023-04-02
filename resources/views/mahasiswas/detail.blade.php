4170846f86d61a597fd01d6c299d2aca58e0bc41
percyfikri <mafikri27@gmail.com>
2023-04-02 11:05:48 +0700

    (views/detail): fix a little correction

--- resources/views/mahasiswas/detail.blade.php
+++ resources/views/mahasiswas/detail.blade.php
@@ -10,16 +10,14 @@
             </div> 
             <div class="card-body"> 
                 <ul class="list-group list-group-flush"> 
-                    <li class="list-group-item"><b>Nim: </b>{{$Mahasiswa>Nim}}</li> 
-                    <li class="list-group-item"><b>Nama: </b>{{$Mahasiswa>Nama}}</li> 
-                    <li class="list-group-item"><b>Kelas: </b>{{$Mahasiswa>Kelas}}</li> 
+                    <li class="list-group-item"><b>Nim: </b>{{$Mahasiswa->Nim}}</li> 
+                    <li class="list-group-item"><b>Nama: </b>{{$Mahasiswa->Nama}}</li> 
+                    <li class="list-group-item"><b>Kelas: </b>{{$Mahasiswa->Kelas}}</li> 
                     <li class="list-group-item"><b>Jurusan: </b>{{$Mahasiswa->Jurusan}}</li> 
-                     <li class="list-group-
-item"><b>No_Handphone: </b>{{$Mahasiswa->No_Handphone}}</li> 
+                     <li class="list-group-item"><b>No_Handphone: </b>{{$Mahasiswa->No_Handphone}}</li> 
                 </ul> 
             </div> 
-            <a class="btn btn-success mt-
-3" href="{{ route('mahasiswas.index') }}">Kembali</a> 
+            <a class="btn btn-success mt-3" href="{{ route('mahasiswas.index') }}">Kembali</a> 
 
         </div> 
     </div> 
